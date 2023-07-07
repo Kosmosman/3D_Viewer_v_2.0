1295929af6c21aae7bea26fa6fa50a2da6cd3b37
stonehom <stonehom@student.21-school.ru>
2023-07-07 19:38:28 +0300

    delete old controller

--- src/controller/controller.h
+++ src/controller/controller.h
@@ -7,19 +7,48 @@
 
 #include <string>
 
+#include "../Model/affine_transformations/affinis.h"
 #include "../Model/basic_model/model.h"
 
 namespace s21 {
 
 class Controller {
  public:
-  Controller() : model_{} {};
-  void InitModel(std::string file);
-  double MaxCoordinate();
-  double* Vertexes();
-  double* Facets();
-  int CountOfFacetsLines();
-  int CountOfVertexes();
+  Controller() = default;
+  void InitModel(std::string file) { model_(file); };
+  double MaxCoordinate() { return model_.GetMaxCoordinate(); };
+  double* Vertexes() { return model_.GetVertexes().data(); };
+  double* Facets() { return model_.GetFacets().data(); };
+  int CountOfFacets() { return model_.GetCountOfVertex(); };
+  int CountOfVertexes() { return model_.GetVertexes().size() / 3; };
+  void MakeMoveX(int value) {
+    AffinisOperation<MoveX> op;
+    op.MakeCalculate(model_.GetVertexes(), value);
+  };
+  void MakeMoveY(int value) {
+    AffinisOperation<MoveY> op;
+    op.MakeCalculate(model_.GetVertexes(), value);
+  };
+  void MakeMoveZ(int value) {
+    AffinisOperation<MoveZ> op;
+    op.MakeCalculate(model_.GetVertexes(), value);
+  };
+  void MakeRotateX(int value) {
+    AffinisOperation<RotateX> op;
+    op.MakeCalculate(model_.GetVertexes(), value);
+  };
+  void MakeRotateY(int value) {
+    AffinisOperation<RotateY> op;
+    op.MakeCalculate(model_.GetVertexes(), value);
+  };
+  void MakeRotateZ(int value) {
+    AffinisOperation<RotateZ> op;
+    op.MakeCalculate(model_.GetVertexes(), value);
+  };
+  void MakeScaling(int value) {
+    AffinisOperation<Scaling> op;
+    op.MakeCalculate(model_.GetVertexes(), value);
+  };
 
  private:
   Model model_;
